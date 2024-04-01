7a3dfe05faeeb861527bdafb9128dabac52f3228
Sujin1517 <11tnwls12@gmail.com>
2024-04-01 16:02:25 +0900

    emotion css study

--- react study/day8style/src/App.jsx
+++ react study/day8style/src/App.jsx
@@ -2,32 +2,35 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { css } from '@emotion/css'
+
+const color = 'white'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [w, setw] = useState("200px");
+  const [h, seth] = useState("80px");
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <input type='range' max="400" min="10" onChange={e => setw(e.target.value + "px")}/>
+      <input type='range' max="400" min="10" onChange={e => seth(e.target.value + "px")}/>
+      <div
+        className={css`
+          width:${w};
+          height:${h};
+          padding: 32px;
+          background-color: hotpink;
+          font-size: 24px;
+          border-radius: 4px;
+          &:hover {
+            transform: rotate(360deg);
+            transition: 1s;
+            animation-direction: alternate;
+            color: ${color};
+        }`}
+      >
+        Hover to change color.
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
